4f2e0b194235a08434b7b860ff33e9acdf524cb9
Hugo Wang <w@mitnk.com>
2016-01-24 18:30:53 +0800

    update config

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os.path
+
 KEY = "0000-0000-0000"
 SERVER_IP = "0.0.0.0"
 SERVER_PORT = 1234
 LOCAL_IP = "127.0.0.1"
 LOCAL_PORT = 1080
+
+if os.path.exists('./config_local.py'):
+    from config_local import *
