42e0f81d4c477ada4020247af9400bdb1a0fdce9
Hugo Wang <w@mitnk.com>
2016-08-29 11:01:48 +0800

    added a cron script

---
+++ cron_local.sh
@@ -0,0 +1,10 @@
+DIR="/Users/hugo/mitnk/lightsocks"
+cd $DIR
+
+ps_count=$(ps ax | grep -w 'local\.py' | wc -l)
+if [ $ps_count -lt 1 ]; then
+    echo "no local.py found, starting it ..."
+    bash ./start_local.sh
+else
+    echo "found local.py"
+fi
