2d661fea6350a9c2cc8d2023dd4964ff68932b78
Hugo Wang <w@mitnk.com>
2016-08-30 09:38:43 +0800

    miscs improve

--- cron_local.sh
+++ cron_local.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 DIR="/Users/hugo/mitnk/lightsocks"
 cd $DIR
 
-ps_count=$(ps ax | grep -w 'local\.py' | wc -l)
+ps_count=$(ps ax | grep -w 'local\.py.*lightsocks.*log' | wc -l)
 if [ $ps_count -lt 1 ]; then
     echo "no local.py found, starting it ..."
     bash ./start_local.sh
